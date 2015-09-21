8387e6b49e89c12be9de291130b0d6030cc639c1
Rajashekar Subramany <rajashekar.subramany@capgemini.com>
2015-09-21 17:24:31 +0530

    Partial

--- webshop-parent/webshop-webapp-admin-spa/src/main/java/com/petsupplies/admin/controller/AbstractController.java
+++ webshop-parent/webshop-webapp-admin-spa/src/main/java/com/petsupplies/admin/controller/AbstractController.java
@@ -1,5 +1,6 @@
 package com.petsupplies.admin.controller;
 
 public class AbstractController {
-
+	public static final String WELCOME = "welcome";
+	public static final String ROOT = "/";
 }
--- webshop-parent/webshop-webapp-admin-spa/src/main/java/com/petsupplies/admin/controller/WelcomeController.java
+++ webshop-parent/webshop-webapp-admin-spa/src/main/java/com/petsupplies/admin/controller/WelcomeController.java
@@ -1,5 +1,27 @@
 package com.petsupplies.admin.controller;
 
-public class WelcomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.petsupplies.model.user.User;
+import com.petsupplies.service.user.IUserService;
+
+@Controller
+public class WelcomeController extends AbstractController {
+
+	@Autowired
+	private IUserService userService;
+
+	@RequestMapping({ ROOT, WELCOME })
+	public String welcome() {
+		return WELCOME;
+	}
+
+	@RequestMapping(value = "/api/users/{username}")
+	public @ResponseBody User getUser(@PathVariable String username) {
+		return userService.findByUsername(username);
+	}
 }
